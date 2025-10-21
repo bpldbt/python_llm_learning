72d3a917fc66e4ac62fcc80967023c54eab62c58
wq <369579819@qq.com>
2025-10-21 17:52:51 +0800

    first commit

--- day2_list_tuple/book_list_manager.py
+++ day2_list_tuple/book_list_manager.py
@@ -18,6 +18,23 @@ fruits.remove("mango")
 print(fruits)
 print(len(fruits))
 
+for fruit in fruits:
+    print(fruit)
+
 for index, fruit in enumerate(fruits):
     print(f"{index}: {fruit}")
     
+squares = [x**2 for x in range(1, 6)]
+print(squares)
+
+numbers = [10, 20, 30, 40, 50, 60, 70, 9]
+even_numbers = [x for x in numbers if x % 2 == 0]
+print(even_numbers)
+
+my_book_list = []
+my_book_list.append("Effective Python")
+my_book_list.append("Clean Code")
+my_book_list.append("The Pragmatic Programmer")
+print("我的图书馆(初始):", my_book_list)
+print("第一本书是:", my_book_list[0])
+print("最后一本书是：", my_book_list[-1])
